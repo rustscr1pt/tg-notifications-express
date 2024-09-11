fc2aa493458add90de537b6a37d2d3ca9601b479
rust_lang_lover <rust_lang_lover@proton.me>
2024-09-11 14:06:00 +0300

    updated .gitignore + .txt files for storing password

--- structs/constants.js
+++ structs/constants.js
@@ -1,4 +1,21 @@
-const token = '*';
-const mysql_password = "*";
+const fs = require('node:fs');
+
+const token = () => {
+    try {
+        return fs.readFileSync('token.txt', 'utf8')
+    }
+    catch (err) {
+        return null
+    }
+}
+
+const mysql_password = () => {
+    try {
+        return fs.readFileSync('password.txt', 'utf8')
+    }
+    catch (err) {
+        return null
+    }
+}
 
 module.exports = {token, mysql_password};
--- telegram/initBot.js
+++ telegram/initBot.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const {token} = require("../structs/constants");
 
 function initBot() {
-    const bot = new TelegramBot(token, {polling: true});
+    const bot = new TelegramBot(token(), {polling: true});
     console.log('Bot for tg is initialized')
     return bot
 }
